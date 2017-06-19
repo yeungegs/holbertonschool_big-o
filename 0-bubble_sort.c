00caf9d6a5303514f5a78d1ce451d0b1bbe5f4b2
Carrie Ybay <carrieybay@gmail.com>
2017-06-19 15:51:08 -0700

    Initial commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,34 @@
-#!/usr/bin/python3
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts array of integers in ascending order
+ * @array: array to sort
+ * @size: size of array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp; /* holds value of array for swapping */
+	int flag; /* checks if a value has been swapped */
+
+	for (i = 0; i < size; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - 1; j++)
+		{
+			/* if current value is larger than the next, swap */
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			break;
+	}
+
+}
