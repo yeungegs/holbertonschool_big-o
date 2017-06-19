0a983fc9271d9b493456a9b4b0cc9ecdf3516a32
Elaine Yeung <yeung.egs@gmail.com>
2017-06-19 07:23:13 +0000

    Update Task 0 - with do while loop implementation! :tada:

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,33 @@
-#!/usr/bin/python3
+#include "sort.h"
+
+/**
+ *bubble_sort - sorts an array of integers in ascending order
+ *using the Bubble sort algorithm
+ *@array:array of integers to sort
+ *@size:size of array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	bool swapped;
+	int i;
+	int temp;
+	int iter = size - 1;
+
+	do {
+		swapped = false;
+		i = 0;
+		for (; i < iter; ++i)
+		{
+			if (array[i + 1] < array[i])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swapped = true;
+				print_array(array, size);
+			}
+		}
+		--iter;
+	} while (swapped);
+}
