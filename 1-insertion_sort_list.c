f6e071202664fb3ae1e8aff3cb526b47dc010b5b
Carrie Ybay <carrieybay@gmail.com>
2017-06-19 17:56:55 -0700

    Initial commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,21 +1,45 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - function that sorts a doubly linked
- * *list of integers in ascending order using the Insertion sort algorithm
- * @list: doubly linked list of integers
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending
+ * order using the Insertion sort algorithm
+ * @list: list to be sorted
+ * Return: void
  */
-
 void insertion_sort_list(listint_t **list)
 {
-/**
- * mark first element as sorted
- * for each unsorted element x
- *   extract the element x
- *   for j = lastSortedIndex down to 0
- *     if current element j > x
- *       move sorted element to the right by 1
- *     break loop and insert x here
- */
+	listint_t *current; /* traverses list */
+	listint_t *temp; /* holds temp values */
 
+	current = *list;
+	current = current->next;
+
+	while (current != NULL)
+	{
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			temp = current;
+
+			/* link next and previous nodes together */
+			if (current->next != NULL)
+				current->next->prev = temp->prev;
+			current->prev->next = temp->next;
+
+			/* move to previous node, insert temp node before it */
+			current = current->prev;
+			temp->prev = current->prev;
+			temp->next = current;
+			if (current->prev != NULL)
+				current->prev->next = temp;
+			current->prev = temp;
+
+			/* if node is new head of list, set it to *list */
+			if (temp->prev == NULL)
+				*list = temp;
+
+			print_list(*list);
+			current = current->prev;
+		}
+		current = current->next;
+	}
 }
